ec9487dc7346c5e46872ec2c1413e5fcb194aa2a
whhit123 <liuyuanlong123@aliyun.com>
2017-05-12 09:57:45 +0800

    add csv file

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -48,6 +51,47 @@ int binSearch(const int *Array,int start,int end,int key)
         }
         return -1;
 }
+inline void csvformat(string& name)  
+{  
+	std::string::size_type startpos = 0;  
+	while (startpos!= std::string::npos)  
+	{  
+		startpos = name.find(',');	 //找到'.'的位置  
+		if( startpos != std::string::npos ) //std::string::npos表示没有找到该字符  
+		{  
+		  name.replace(startpos,1," "); //实施替换，注意后面一定要用""引起来，表示字符串  
+		}  
+	}  
+}  
+int p[100];
+int len = 0;
+void datafromtxt(string bookfilename)
+{
+
+	ifstream inFile;
+	int* temp = p;
+	inFile.open(bookfilename.c_str());
+	if(!inFile) 
+	{
+		cout<<"File open fail"<<endl;
+		return;
+	}
+	string line,word;
+	while(getline(inFile,line))
+	{
+		#if 1
+		csvformat(line);
+		#endif
+		int tempnode;
+		
+		istringstream record(line);
+		record>>tempnode;
+		cout<<tempnode<<endl;
+		*temp++ = tempnode;
+		len++;
+	}
+	inFile.close();
+}
 
 int main()
 {
@@ -56,7 +100,11 @@ int main()
 	cout <<binSearch(a,0,9,1)<<endl;
 	int b[10] = {10,9,8,7,6,5,4,3,2,1};
 	int *c = quickSort(b,10);
-	cout<<binSearch(c,0,9,1);
-	
-	
+	cout<<binSearch(c,0,9,1)<<endl;
+
+	datafromtxt("1.csv");
+
+	int *d = quickSort(p,len);
+	cout<<binSearch(d,0,len-1,1)<<endl;
+	system("PAUSE");
 }
